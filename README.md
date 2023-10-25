2ec1b443637cd4251607783406ecb411ead83242
Kosuke July Hata <july@faust.ooo>
2023-10-24 22:25:44 -0700

    adding basic ros2 msgs

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Geist common
+
+### how to get started
+
+Make sure you update the submodules:
+
+```
+❯ git submodule update --init --recursive                            
+```
+
+We use 
+- https://github.com/ros2/common_interfaces
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,7 @@
 use roslibrust_codegen_macro::find_and_generate_ros_messages;
 
 find_and_generate_ros_messages!(
-    "./common", 
-    "./humble/share/std_msgs"
+    "assets/common",
+    "assets/ros2",
 );
 
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
