156c91c32cc7919a0b223050a17554ac66654808
Christopher Di Santo <disanto@gmail.com>
2023-11-08 03:57:22 +0100

    newly generated msgs

--- src/generated/msgs.ts
+++ src/generated/msgs.ts
@@ -1,5 +1,162 @@
 /* eslint-disable */
 // These files were generated using "ros-typescript-generator"
+export interface IGeometryMsgsAccel {
+  linear: IGeometryMsgsVector3;
+  angular: IGeometryMsgsVector3;
+}
+
+export interface IGeometryMsgsAccelStamped {
+  header: IStdMsgsHeader;
+  accel: IGeometryMsgsAccel;
+}
+
+export interface IGeometryMsgsAccelWithCovariance {
+  accel: IGeometryMsgsAccel;
+  covariance: number[];
+}
+
+export interface IGeometryMsgsAccelWithCovarianceStamped {
+  header: IStdMsgsHeader;
+  accel: IGeometryMsgsAccelWithCovariance;
+}
+
+export interface IGeometryMsgsInertia {
+  m: number;
+  com: IGeometryMsgsVector3;
+  ixx: number;
+  ixy: number;
+  ixz: number;
+  iyy: number;
+  iyz: number;
+  izz: number;
+}
+
+export interface IGeometryMsgsInertiaStamped {
+  header: IStdMsgsHeader;
+  inertia: IGeometryMsgsInertia;
+}
+
+export interface IGeometryMsgsPoint {
+  x: number;
+  y: number;
+  z: number;
+}
+
+export interface IGeometryMsgsPoint32 {
+  x: number;
+  y: number;
+  z: number;
+}
+
+export interface IGeometryMsgsPointStamped {
+  header: IStdMsgsHeader;
+  point: IGeometryMsgsPoint;
+}
+
+export interface IGeometryMsgsPolygon {
+  points: IGeometryMsgsPoint32[];
+}
+
+export interface IGeometryMsgsPolygonStamped {
+  header: IStdMsgsHeader;
+  polygon: IGeometryMsgsPolygon;
+}
+
+export interface IGeometryMsgsPose {
+  position: IGeometryMsgsPoint;
+  orientation: IGeometryMsgsQuaternion;
+}
+
+export interface IGeometryMsgsPose2D {
+  x: number;
+  y: number;
+  theta: number;
+}
+
+export interface IGeometryMsgsPoseArray {
+  header: IStdMsgsHeader;
+  poses: IGeometryMsgsPose[];
+}
+
+export interface IGeometryMsgsPoseStamped {
+  header: IStdMsgsHeader;
+  pose: IGeometryMsgsPose;
+}
+
+export interface IGeometryMsgsPoseWithCovariance {
+  pose: IGeometryMsgsPose;
+  covariance: number[];
+}
+
+export interface IGeometryMsgsPoseWithCovarianceStamped {
+  header: IStdMsgsHeader;
+  pose: IGeometryMsgsPoseWithCovariance;
+}
+
+export interface IGeometryMsgsQuaternion {
+  x: number;
+  y: number;
+  z: number;
+  w: number;
+}
+
+export interface IGeometryMsgsQuaternionStamped {
+  header: IStdMsgsHeader;
+  quaternion: IGeometryMsgsQuaternion;
+}
+
+export interface IGeometryMsgsTransform {
+  translation: IGeometryMsgsVector3;
+  rotation: IGeometryMsgsQuaternion;
+}
+
+export interface IGeometryMsgsTransformStamped {
+  header: IStdMsgsHeader;
+  child_frame_id: string;
+  transform: IGeometryMsgsTransform;
+}
+
+export interface IGeometryMsgsTwist {
+  linear: IGeometryMsgsVector3;
+  angular: IGeometryMsgsVector3;
+}
+
+export interface IGeometryMsgsTwistStamped {
+  header: IStdMsgsHeader;
+  twist: IGeometryMsgsTwist;
+}
+
+export interface IGeometryMsgsTwistWithCovariance {
+  twist: IGeometryMsgsTwist;
+  covariance: number[];
+}
+
+export interface IGeometryMsgsTwistWithCovarianceStamped {
+  header: IStdMsgsHeader;
+  twist: IGeometryMsgsTwistWithCovariance;
+}
+
+export interface IGeometryMsgsVector3 {
+  x: number;
+  y: number;
+  z: number;
+}
+
+export interface IGeometryMsgsVector3Stamped {
+  header: IStdMsgsHeader;
+  vector: IGeometryMsgsVector3;
+}
+
+export interface IGeometryMsgsWrench {
+  force: IGeometryMsgsVector3;
+  torque: IGeometryMsgsVector3;
+}
+
+export interface IGeometryMsgsWrenchStamped {
+  header: IStdMsgsHeader;
+  wrench: IGeometryMsgsWrench;
+}
+
 export interface IStdMsgsBool {
   data: boolean;
 }
@@ -246,4 +403,92 @@ export interface IUndefinedTime {
 export interface IUndefinedWhoAmIResponse {
   success: boolean;
   version: string;
+}
+
+export interface IVisionMsgsBoundingBox2D {
+  center: IVisionMsgsPose2D;
+  size_x: number;
+  size_y: number;
+}
+
+export interface IVisionMsgsBoundingBox2DArray {
+  header: IStdMsgsHeader;
+  boxes: IVisionMsgsBoundingBox2D[];
+}
+
+export interface IVisionMsgsBoundingBox3D {
+  center: IGeometryMsgsPose;
+  size: IGeometryMsgsVector3;
+}
+
+export interface IVisionMsgsBoundingBox3DArray {
+  header: IStdMsgsHeader;
+  boxes: IVisionMsgsBoundingBox3D[];
+}
+
+export interface IVisionMsgsClassification {
+  header: IStdMsgsHeader;
+  results: IVisionMsgsObjectHypothesis[];
+}
+
+export interface IVisionMsgsDetection2D {
+  header: IStdMsgsHeader;
+  results: IVisionMsgsObjectHypothesisWithPose[];
+  bbox: IVisionMsgsBoundingBox2D;
+  id: string;
+}
+
+export interface IVisionMsgsDetection2DArray {
+  header: IStdMsgsHeader;
+  detections: IVisionMsgsDetection2D[];
+}
+
+export interface IVisionMsgsDetection3D {
+  header: IStdMsgsHeader;
+  results: IVisionMsgsObjectHypothesisWithPose[];
+  bbox: IVisionMsgsBoundingBox3D;
+  id: string;
+}
+
+export interface IVisionMsgsDetection3DArray {
+  header: IStdMsgsHeader;
+  detections: IVisionMsgsDetection3D[];
+}
+
+export interface IVisionMsgsLabelInfo {
+  header: IStdMsgsHeader;
+  class_map: IVisionMsgsVisionClass[];
+  threshold: number;
+}
+
+export interface IVisionMsgsObjectHypothesis {
+  class_id: string;
+  score: number;
+}
+
+export interface IVisionMsgsObjectHypothesisWithPose {
+  hypothesis: IVisionMsgsObjectHypothesis;
+  pose: IGeometryMsgsPoseWithCovariance;
+}
+
+export interface IVisionMsgsPoint2D {
+  x: number;
+  y: number;
+}
+
+export interface IVisionMsgsPose2D {
+  position: IVisionMsgsPoint2D;
+  theta: number;
+}
+
+export interface IVisionMsgsVisionClass {
+  class_id: number;
+  class_name: string;
+}
+
+export interface IVisionMsgsVisionInfo {
+  header: IStdMsgsHeader;
+  method: string;
+  database_location: string;
+  database_version: number;
 }
