990cbc87ded31f459b73d675146a90cdba5c96c6
Kosuke Hata <hata.kosuke@gmail.com>
2023-11-17 10:48:21 -0800

    v0.1.13

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,11 +3,9 @@
 set -e
 
 # This creates a rust package on crates.io
-pushd rust
 ./scripts/publish.sh
-popd
 
 # this creates a typescript npm package
 pushd ts
-# ./scripts/publish.sh
+./scripts/publish.sh
 popd
